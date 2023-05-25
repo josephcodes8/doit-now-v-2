5f69db5df7c9fc4ac5d1d653048fd1939526943b
josephcodes8 <josephworku23@gmail.com>
2023-05-25 15:39:08 -0700

    changed the padding of some elements

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,12 +1,12 @@
 import { successIcon } from "../assets";
 const Notification = () => {
   return (
-    <div className="my-5 mx-10 flex gap-x-5 bg-green-50 p-4 rounded-xl">
+    <div className="m-5 flex gap-x-5 bg-green-50 p-4 rounded-xl">
       {/* icon */}
       <img src={successIcon} alt="" width={50} />
       <div>
         <h1 className="text-xl font-bold mb-2 text-green-900">Successfull</h1>
-        <p className="text-sm md:text-base text-gray-500">
+        <p className="text-xs md:text-base text-gray-500">
           You successfully added a task
         </p>
       </div>
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,7 +2,7 @@ import Task from "./Task";
 import { Tasks } from "../data";
 const TaskList = () => {
   return (
-    <main className="font-light bg-slate-950 mt-2 px-10 py-8">
+    <main className="font-light bg-slate-950 mt-2 px-5 py-8">
       <h1 className="text-white text-xl font-bold">Tasks to Complete</h1>
       {Tasks.map((task) => (
         <Task key={task.id} {...task} />
